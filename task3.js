730f9720334f1cff868a57404a3e91abc6d5add6
Fliss317 <felicity.cullen@mail.com>
2022-11-08 12:30:12 +0000

    Working on Task 3

--- task3.js
+++ task3.js
@@ -1,1 +1,17 @@
 let someNumbers = [4, 5, 6, 78, 2, 3, 45, 34, 2, 23, 5, 45, 6, 7, 23];
+
+// PLAN 
+// four new variables for 3, 5, 60, 90 
+// check each is true or false depending on if multiple is present in someNumbers 
+// use .some to check
+
+let threeInArray = someNumbers.some((number)=>{
+    let checkNumber = number % 3;
+    if (checkNumber === true) {
+        return true; 
+    } else {
+        return false;
+    }
+});
+
+console.log(threeInArray);
